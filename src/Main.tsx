b3f709c0e52e5b445c8a327b60ef6aab2ffa1367
Antonin <antonin.morvan@gmail.com>
2019-11-23 11:20:52 +0100

    Add suspense and lazy behaviour

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useGet } from 'restful-react';
 import Loadable from 'react-loadable';
 
-const LoadableComponent = Loadable({
+/**
+ * 1
+ * Example dynamic import with react-loadable
+ */
+/* const LoadableComponent = Loadable({
     loader: () => import('./ButtonComponent'),
     loading : () => <div>loading ...</div>
 })
@@ -13,6 +17,26 @@ const Main = () => {
             <LoadableComponent />
         </div>
     )
+} */
+
+/**
+ * 2
+ * Example dynamic import with Suspense and React.lazy
+ * Suspense used with fallback prop, while component is loading
+ */
+
+const ButtonComponent = React.lazy(() => import('./ButtonComponent'));
+const Main = () => {
+    return (
+        <div>
+            <h1>My Component</h1>
+            <Suspense fallback={<div>Loading ...</div>}>
+                <ButtonComponent />
+            </Suspense>
+        </div>
+    )
 }
+
+
 
 export default Main;
